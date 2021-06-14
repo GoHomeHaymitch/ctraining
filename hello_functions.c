83c1749846852893e7a9561a7e323ed0d9fa3feb
GoHomeHaymitch <71917882+GoHomeHaymitch@users.noreply.github.com>
2021-06-14 09:34:13 -0500

    Update hello_functions.c

--- hello_functions.c
+++ hello_functions.c
@@ -24,11 +24,32 @@ float temp_sensor_converter(int temp_sensor_value, bool *success)
   {
     //change success to false
     printf("Temperature sensor value (%d) is not in the range of %d to %d, changing success to false.\n", temp_sensor_value, TEMP_SENSE_MIN, TEMP_SENSE_MAX);
-    //slope of a line with endpoints (TEMP_SENSE_MIN, TEMP_C_MIN) and (TEMP_SENSE_MAX, TEMP_C_MAX) where temp_sense is on the x axis and temp_c is on the y axis
-    float TEMP_SLOPE = (TEMP_C_MAX - TEMP_C_MIN) / (1.0 * (TEMP_SENSE_MAX - TEMP_SENSE_MIN));
+    *success = false;
+    return -100;
+  }
+  
+  printf("Temperature sensor value (%d) is in the range of %d to %d\n Attempting conversion now ...\n", temp_sensor_value, TEMP_SENSE_MIN, TEMP_SENSE_MAX);
+  //slope of a line with endpoints (TEMP_SENSE_MIN, TEMP_C_MIN) and (TEMP_SENSE_MAX, TEMP_C_MAX) where temp_sense is on the x axis and temp_c is on the y axis
+  float TEMP_SLOPE = (TEMP_C_MAX - TEMP_C_MIN) / (1.0 * (TEMP_SENSE_MAX - TEMP_SENSE_MIN));
     
-    //y=mx+b, y is the temp_c, m is the TEMP_SLOPE, x is the temp_sense, and b is the TEMP_C_Min
-    temp_celsius  = TEMP_SLOPE * temp_sensor_value + TEMP_C_MIN;
+  //y=mx+b, y is the temp_c, m is the TEMP_SLOPE, x is the temp_sense, and b is the TEMP_C_Min
+  temp_celsius  = TEMP_SLOPE * temp_sensor_value + TEMP_C_MIN;
     
-    if(temp_celsius < TEMP_C_MIN || temp_celsius > TEMP_C_MAX)
-    {
+  if(temp_celsius < TEMP_C_MIN || temp_celsius > TEMP_C_MAX)
+  {
+    printf("Temperature celsius value (%f) is not in the range of %f to %f, changing success to false.\n", temp_celsius, TEMP_C_MIN, TEMP_C_MAX);
+    *success = false;
+    return -200;
+  }
+  
+  printf("Temperature sensor value (%d) was converted sucessfully to temperature %f C in the range of %f C to %f C.\n", temp_sensor_value, temp_celsius, TEMP_C_MIN, TEMP_C_MAX);
+  *success = true;
+  return temp_celsius;
+}
+
+float temp_c_to_f(float temp_c)
+{
+  float temp_f = (9.0 / 5.0) * temp_c + 32.0;
+  printf("Temperature %f C = %f F\n", temp_c, temp_f);
+  return temp_f;
+}
